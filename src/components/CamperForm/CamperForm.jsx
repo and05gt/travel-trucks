db9fd076575a4f3d9fcb29611a856b9700d54d07
Andrii D. <and05gt@gmail.com>
2025-02-19 23:24:16 +0100

    Added form validation and calendar

--- src/components/CamperForm/CamperForm.jsx
+++ src/components/CamperForm/CamperForm.jsx
@@ -1,10 +1,33 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import './DatePicker.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import style from './CamperForm.module.css';
 
+const initialValues = {
+  name: '',
+  email: '',
+  date: null,
+  comment: '',
+};
+
+const formSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Name is required'),
+  email: Yup.string()
+    .email('Must be a valid email!')
+    .required('Email is required'),
+  date: Yup.date().required('Booking date is required'),
+  comment: Yup.string(),
+});
+
 const CamperForm = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = (values, options) => {
     iziToast.success({
       title: 'Ok',
       titleColor: '#fff',
@@ -14,48 +37,85 @@ const CamperForm = () => {
       messageColor: '#fff',
       theme: 'dark',
     });
-    event.target.reset();
+    options.resetForm();
   };
 
   return (
-    <div className={style.formContainer}>
+    <div className={style.formSection}>
       <div className={style.formWrapper}>
         <h3 className={style.formTitle}>Book your campervan now</h3>
         <p className={style.formSubtitle}>
           Stay connected! We are always ready to help you.
         </p>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          className={style.formInput}
-          type="text"
-          name="name"
-          placeholder="Name*"
-          required
-        />
-        <input
-          className={style.formInput}
-          type="email"
-          name="email"
-          placeholder="Email*"
-          required
-        />
-        <input
-          className={style.formInput}
-          type="text"
-          name="text"
-          placeholder="Booking date*"
-          required
-        />
-        <textarea
-          className={style.formComment}
-          name="comment"
-          placeholder="Comment"
-        ></textarea>
-        <button className={style.formBtn} type="submit">
-          Send
-        </button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={formSchema}
+      >
+        <Form>
+          <div className={style.formContainer}>
+            <label>
+              <Field
+                className={style.formInput}
+                type="text"
+                name="name"
+                placeholder="Name*"
+              />
+              <ErrorMessage
+                name="name"
+                component="span"
+                className={style.formError}
+              />
+            </label>
+            <label>
+              <Field
+                className={style.formInput}
+                type="email"
+                name="email"
+                placeholder="Email*"
+              />
+              <ErrorMessage
+                name="email"
+                component="span"
+                className={style.formError}
+              />
+            </label>
+            <label>
+              <Field type="text" name="date">
+                {({ field, form }) => (
+                  <DatePicker
+                    {...field}
+                    selected={field.value ? new Date(field.value) : null}
+                    onChange={(date) => form.setFieldValue('date', date)}
+                    // minDate={new Date()}
+                    dateFormat="dd.MM.yyyy"
+                    shouldCloseOnSelect={true}
+                    placeholderText="Booking date*"
+                    className={style.formInput}
+                  />
+                )}
+              </Field>
+              <ErrorMessage
+                name="date"
+                component="span"
+                className={style.formError}
+              />
+            </label>
+            <label>
+              <Field
+                className={style.formComment}
+                as="textarea"
+                name="comment"
+                placeholder="Comment"
+              />
+            </label>
+          </div>
+          <button className={style.formBtn} type="submit">
+            Send
+          </button>
+        </Form>
+      </Formik>
     </div>
   );
 };
